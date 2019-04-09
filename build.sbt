ddf273ebc6fce82723397a9874cf6cbe61548a0d
Dan <daniel.alvey@digital.hmrc.gov.uk>
2019-04-09 16:10:22 +0100

    Uploaded simple Travel Expenses calculation tool.

---
+++ build.sbt
@@ -0,0 +1,5 @@
+name := "DTA Calculator"
+
+version := "0.1"
+
+scalaVersion := "2.12.8"
---
+++ src/main/scala/Application.scala
@@ -0,0 +1,36 @@
+import scala.io.StdIn
+
+object Application extends App {
+
+  val travelledDays = StdIn.readLine("Enter Number of days travelled:  \n")
+
+  val mileage = 0.25 * 46
+  val tyneTunnel = 1.70 * 2
+  val fourLaneEnds = 0.9
+
+  def calculateMileage(days: String): String = {
+
+    f"Days travelled: $days.\n \nMileage @ (0.25p per mile x 46 miles) = £$mileage%1.2f. " +
+      f" \n£$mileage%1.2f x $days days = £${mileage * days.toInt}%1.2f\n"
+      }
+
+  def calculateTolls(days: String): String = {
+
+    f"Tyne Tunnel @ (£1.70 x 2) per day. \n£$tyneTunnel%1.2f * $days days = £${tyneTunnel * days.toInt}%1.2f\n"
+  }
+
+  def calculateParking(days: String): String = {
+    f"Parking @ ($fourLaneEnds%1.2f per day). \n£$fourLaneEnds%1.2f x $days = £${fourLaneEnds * days.toInt}%1.2f\n"
+  }
+
+    def calculateExpenses(days : String, sum : Int => String) : String = {
+      sum(days.toInt)
+    }
+
+  println(calculateMileage(travelledDays))
+  println(calculateTolls(travelledDays))
+  println(calculateParking(travelledDays))
+  println(calculateExpenses(travelledDays, days => {
+    f"Total expenses for $days days. = £${(mileage + tyneTunnel + fourLaneEnds) * days}%1.2f"
+  }))
+}
